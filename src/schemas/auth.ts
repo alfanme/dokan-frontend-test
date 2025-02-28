ce2ea92d84c6240580474c4a05b9f20b08149990
Ahmad Alfan <aalf4nz@gmail.com>
2025-02-28 21:48:06 +0700

    feat: create login data schema and type

---
+++ src/schemas/auth.ts
@@ -0,0 +1,15 @@
+import { z } from 'zod';
+
+export const loginSchema = z.object({
+  email: z.string().email('Invalid email format'),
+  password: z
+    .string()
+    .min(8, 'Password must be at least 8 characters')
+    .max(64, 'Password must be at most 64 characters')
+    .regex(/[A-Z]/, 'Password must contain at least one uppercase letter')
+    .regex(/[a-z]/, 'Password must contain at least one lowercase letter')
+    .regex(/[0-9]/, 'Password must contain at least one number')
+    .regex(/[\W_]/, 'Password must contain at least one special character'),
+});
+
+export type Login = z.infer<typeof loginSchema>;
